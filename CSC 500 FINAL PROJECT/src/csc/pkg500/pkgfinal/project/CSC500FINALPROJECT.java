bf3b06491b41185f89fc3f959543595a4667e491
jefflutz1 <jlutz155@gmail.com>
2017-11-02 15:18:08 -0700

    Final project java

---
+++ CSC 500 FINAL PROJECT/src/csc/pkg500/pkgfinal/project/CSC500FINALPROJECT.java
@@ -0,0 +1,16 @@
+package csc.pkg500.pkgfinal.project;
+
+/**
+ *
+ * @author Capitalism
+ */
+public class CSC500FINALPROJECT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
