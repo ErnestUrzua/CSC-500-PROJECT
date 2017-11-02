d722065e58ef0d51bdf12fa5f0f77c95b520d64a
jefflutz1 <jlutz155@gmail.com>
2017-11-02 15:20:09 -0700

    Added important stuff

--- CSC 500 FINAL PROJECT/src/csc/pkg500/pkgfinal/project/CSC500FINALPROJECT.java
+++ CSC 500 FINAL PROJECT/src/csc/pkg500/pkgfinal/project/CSC500FINALPROJECT.java
@@ -1,16 +1,11 @@
 package csc.pkg500.pkgfinal.project;
 
-/**
- *
- * @author Capitalism
- */
+
 public class CSC500FINALPROJECT {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        hey don
     }
     
 }
